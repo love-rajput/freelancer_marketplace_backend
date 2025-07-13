c08c3338f8cd6538b6f14ae1ca238feeb2046fbc
Jay Thakor <jaysinhdarbar730@gmail.com>
2025-07-13 19:20:11 +0530

    Search and Filter is working now

--- routes/gigsRoutes.js
+++ routes/gigsRoutes.js
@@ -8,13 +8,14 @@ const {
   updateGig,
   deleteGig,
   getMyGigs,
+  getOtherFreelancerGigs,
 } = require("../controllers/gigsController");
 const upload = require("../middleware/uploadCloudinary");
 
 router.post("/create", authMiddleware, upload.single("thumbnail"), createGig);
 router.get("/", getGigs);
 router.get("/my-gigs", authMiddleware, getMyGigs);
-router,get("/others", authMiddleware, getOtherFreelancerGigs);
+router.get("/others", authMiddleware, getOtherFreelancerGigs);
 router.get("/:id", getGig);
 router.put("/:id", authMiddleware, upload.single("thumbnail"), updateGig);
 router.delete("/:id", authMiddleware, deleteGig);
